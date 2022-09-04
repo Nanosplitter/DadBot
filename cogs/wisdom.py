b5d26d1a7fc247911bc87d3674c3ffac7926ee28
Colin Wilson <github@colinwilson.dev>
2022-09-04 03:09:29 -0400

    Update wisdom to use slash commands

--- cogs/wisdom.py
+++ cogs/wisdom.py
@@ -4,7 +4,11 @@ import aiohttp
 import nextcord
 import requests
 import yaml
+import nextcord
+from typing import Optional
 from nextcord.ext import commands
+from nextcord import Interaction, SlashOption, ChannelType
+from nextcord.abc import GuildChannel
 import inspirobot
 
 if "DadBot" not in str(os.getcwd()):
@@ -17,8 +21,8 @@ class Wisdom(commands.Cog, name="wisdom"):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="randomfact")
-    async def randomfact(self, context):
+    @nextcord.slash_command(name="randomfact", description="Dad has learned a few things, he'll share.")
+    async def randomfact(self, interaction: Interaction):
         """
         [No Arguments] Dad has learned a few things, he'll share.
         """
@@ -28,25 +32,25 @@ class Wisdom(commands.Cog, name="wisdom"):
                 if request.status == 200:
                     data = await request.json()
                     embed = nextcord.Embed(description=data["text"], color=config["main_color"])
-                    await context.send(embed=embed)
+                    await interaction.response.send_message(embed=embed)
                 else:
                     embed = nextcord.Embed(
                         title="Error!",
                         description="There is something wrong with the API, please try again later",
                         color=config["error"]
                     )
-                    await context.reply(embed=embed)
+                    await interaction.response.send_message(embed=embed)
     
-    @commands.command(name="inspire")
-    async def inspire(self, context):
+    @nextcord.slash_command(name="inspire", description="Get an inspirational poster courtesy of https://inspirobot.me/")
+    async def inspire(self, interaction: Interaction):
         """
         [No Arguments] Get an inspirational poster courtesy of https://inspirobot.me/
         """
         quote = inspirobot.generate()
-        await context.reply(quote.url)
+        await interaction.response.send_message(quote.url)
     
-    @commands.command(name="wisdom")
-    async def wisdom(self, context):
+    @nextcord.slash_command(name="wisdom", description="Get some wisdom courtesy of https://inspirobot.me/")
+    async def wisdom(self, interaction: Interaction):
         """
         [No Arguments] Get some wisdom courtesy of https://inspirobot.me/
         """
@@ -55,15 +59,15 @@ class Wisdom(commands.Cog, name="wisdom"):
         for quote in flow:
             res += quote.text + "\n"
         
-        await context.reply(res)
+        await interaction.response.send_message(res)
 
-    @commands.command(name="advice")
-    async def advice(self, context):
+    @nextcord.slash_command(name="advice", description="Get some fatherly advice.")
+    async def advice(self, interaction: Interaction):
         """
         [No Arguments] Get some fatherly advice.
         """
         r = requests.get("https://api.adviceslip.com/advice")
-        await context.reply(r.json()['slip']['advice'])
+        await interaction.response.send_message(r.json()['slip']['advice'])
 
 # And then we finally add the cog to the bot so that it can load, unload, reload and use it's content.
 def setup(bot):

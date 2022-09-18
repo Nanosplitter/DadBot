780ad89c7e2e981a4b865d5aa0414aed12552fa9
Colin Wilson <github@colinwilson.dev>
2022-09-18 10:57:41 -0400

    Add emojitype command

--- cogs/memes.py
+++ cogs/memes.py
@@ -53,8 +53,59 @@ class Memes(commands.Cog, name="memes"):
         for word in message.content.split(" "):
             res += word + (" " + random.choice(self.emoji_mappings[word.lower()]) + " " if word in self.emoji_mappings else " ")
         await interaction.response.send_message(res)
-        
+    
+    @nextcord.slash_command(name="emojitype", description="Make a message with letter emojis")
+    async def nobitches(self, interaction: Interaction, text: str = SlashOption(description="Message to send", required=True)):
+        """
+        [Text] Make a message with letter emojis
+        """
+        emoji_mappings = {
+            "a": ":regional_indicator_a:",
+            "b": ":regional_indicator_b:",
+            "c": ":regional_indicator_c:",
+            "d": ":regional_indicator_d:",
+            "e": ":regional_indicator_e:",
+            "f": ":regional_indicator_f:",
+            "g": ":regional_indicator_g:",
+            "h": ":regional_indicator_h:",
+            "i": ":regional_indicator_i:",
+            "j": ":regional_indicator_j:",
+            "k": ":regional_indicator_k:",
+            "l": ":regional_indicator_l:",
+            "m": ":regional_indicator_m:",
+            "n": ":regional_indicator_n:",
+            "o": ":regional_indicator_o:",
+            "p": ":regional_indicator_p:",
+            "q": ":regional_indicator_q:",
+            "r": ":regional_indicator_r:",
+            "s": ":regional_indicator_s:",
+            "t": ":regional_indicator_t:",
+            "u": ":regional_indicator_u:",
+            "v": ":regional_indicator_v:",
+            "w": ":regional_indicator_w:",
+            "x": ":regional_indicator_x:",
+            "y": ":regional_indicator_y:",
+            "z": ":regional_indicator_z:",
+            " ": "   ",
+            "1": ":regional_indicator_one:",
+            "2": ":regional_indicator_two:",
+            "3": ":regional_indicator_three:",
+            "4": ":regional_indicator_four:",
+            "5": ":regional_indicator_five:",
+            "6": ":regional_indicator_six:",
+            "7": ":regional_indicator_seven:",
+            "8": ":regional_indicator_eight:",
+            "9": ":regional_indicator_nine:",
+            "!": ":grey_exclamation:",
+            "?": ":grey_question:",
+            "#": ":hash:",
+            "*": ":asterisk:"
+        }
 
+        res = [f"{emoji_mappings[letter]}" if letter in emoji_mappings else letter for letter in text.lower()]
+
+        await interaction.response.send_message("".join(res))
+                
 # And then we finally add the cog to the bot so that it can load, unload, reload and use it's content.
 def setup(bot):
     bot.add_cog(Memes(bot))

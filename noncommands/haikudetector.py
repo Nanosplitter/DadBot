eb101dad026ea9f969fbec97907e601d743ce3a0
Colin Wilson <github@colinwilson.dev>
2022-11-15 04:05:45 +0000

    FIX: Fix haiku recognition (#6)
    * Start haiku refactor
    
    * Fix haikus

--- noncommands/haikudetector.py
+++ noncommands/haikudetector.py
@@ -2,54 +2,49 @@ import re
 import yaml
 import sys
 import os
-import cmudict
-
-cd = cmudict.dict()
+import syllapy
 
 with open("config.yaml") as file:
     config = yaml.load(file, Loader=yaml.FullLoader)
 
 
-def lookup_word(word):
-    return cd.get(word)
-
-def sylcoOneWord(word):
-    count = 0
-    phones = lookup_word(word) # this returns a list of matching phonetic rep's
-    if phones:                   # if the list isn't empty (the word was found)
-        phones0 = phones[0]      #     process the first
-        count = len([p for p in phones0 if p[-1].isdigit()]) # count the vowels
-    return count
-
-def sylco(words):
-    res = sum([sylcoOneWord(re.sub(r'[^\w\s]', '', w).lower()) for w in words.split()])
-    return res
-
-
-
-def popNumSyl(syl, words):
-    res = []
-    while syl > 0 and len(words) > 0:
-        word = words.pop()
-        res.append(word)
-        syl -= sylco(word)
-
-    return (syl == 0, res, words)
 
 class HaikuDetector:
+
+    # Detect a haiku in the message
     async def checkForHaiku(self, message):
         text = message.content
         words = text.split()[::-1]
-        poem = []
-        if sylco(text) == 17:
-            lines = [5, 7, 5]
-            for syl in lines:
-                res = popNumSyl(syl, words)
-                words = res[2].copy()
-                poem.append(res)
-            if all([i[0] for i in poem]):
-                res = "You're a poet!\n\n"
-                for line in [i[1] for i in poem]:
-                    res += "*" + " ".join(line) + "*\n"
-                res += "\n -" + message.author.mention
-                await message.reply(res)
+        if len(words) < 3:
+            return False
+        if len(words) > 17:
+            return False
+        
+        line1 = 5
+        line2 = 7
+        line3 = 5
+        line1words = []
+        line2words = []
+        line3words = []
+
+        for word in words:
+            if (syllapy.count(word) == 0):
+                return False
+            if line1 > 0:
+                line1words.append(word)
+                line1 -= syllapy.count(word)
+            elif line2 > 0:
+                line2words.append(word)
+                line2 -= syllapy.count(word)
+            elif line3 > 0:
+                line3words.append(word)
+                line3 -= syllapy.count(word)
+            else:
+                break
+
+        if line1 == 0 and line2 == 0 and line3 == 0 and len(line1words) + len(line2words) + len(line3words) == len(words):
+            await message.channel.send(f"You're a poet!\n*{' '.join(line1words[::-1])}\n{' '.join(line2words[::-1])}\n{' '.join(line3words[::-1])}*\n- {message.author.mention}")
+            return True
+        return False
+        
+        

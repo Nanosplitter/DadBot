264110749c367cd392b2fbed04963ee5a07b5ffa
Colin Wilson <github@colinwilson.dev>
2022-12-02 16:46:42 -0500

    Fix 1d0 bug

--- cogs/dnd.py
+++ cogs/dnd.py
@@ -104,7 +104,7 @@ class DnD(commands.Cog, name="dnd"):
         dice = dice.lower()
         dice = dice.replace(" ", "")
 
-        match = re.findall(r"^([+]?[1-9]\d*|0)?d([+]?[1-9]\d*|0)([+-][+-]?[1-9]\d*|0)?", dice)
+        match = re.findall(r"^([+]?[1-9]\d*|0)?d([+]?[1-9]\d*)([+-][+-]?[1-9]\d*|0)?", dice)
 
         if len(match) == 0 or "." in dice:
             await interaction.response.send_message("Invalid dice roll, make sure it has only integers and is something like `3d6+5`, or `2d6`, or `d20`, or `d20+5`", ephemeral=True)

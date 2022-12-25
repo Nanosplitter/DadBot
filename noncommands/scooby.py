a438568f2b0baec53f4b241454bb017bfd873e73
Colin Wilson <github@colinwilson.dev>
2022-12-24 21:17:14 -0500

    Fix apod

--- noncommands/scooby.py
+++ noncommands/scooby.py
@@ -4,11 +4,13 @@ from nextcord import Embed
 class Scooby:
     def __init__(self, bot):
         self.bot = bot
+        
     async def apod(self):
         c = self.bot.get_channel(856919399789625376)
-        response = requests.get("https://api.nasa.gov/planetary/apod")
-        await c.send(embed=Embed(title="Astronomy Picture of the Day", description=response.json()["explanation"]))
-        await c.send(response["hdurl"])
+        response = requests.get("https://api.nasa.gov/planetary/apod?api_key=hQqgupM0Ghb1OTjjrPkoIDw1EJq6pZQQdgMGBpnb")
+        embed = Embed(title="Astronomy Picture of the Day", description=response.json()["explanation"])
+        embed.set_image(url=response.json()["hdurl"])
+        await c.send(embed=embed)
     
     async def praiseFireGator(self):
         c = self.bot.get_channel(856919399789625376)

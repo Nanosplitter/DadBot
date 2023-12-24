ef496d669e2afa7c4b52760bbab5f0a4d29a9d7c
Colin Wilson <github@colinwilson.dev>
2023-12-24 01:17:52 -0500

    feat: :sparkles: Make eight ball better with AI

--- cogs/fun.py
+++ cogs/fun.py
@@ -17,6 +17,8 @@ from typing import Optional
 from nextcord.ext import commands
 from nextcord import Interaction, SlashOption, Embed
 
+from noncommands.dadroid import dadroid_single
+
 
 with open("config.yaml") as file:
     config = yaml.load(file, Loader=yaml.FullLoader)
@@ -27,84 +29,137 @@ class Fun(commands.Cog, name="fun"):
         self.bot = bot
         with open("./resources/emoji-mappings.json", encoding="utf8") as file:
             self.emoji_mappings = json.load(file)
-    
-    @nextcord.slash_command(name="dadjoke", description="Have Dad tell you one of his classics.")
-    async def dadjoke(self, interaction: Interaction, searchterm: Optional[str] = SlashOption(description="A term to try and find a dadjoke about", default="", required=False)):
+
+    @nextcord.slash_command(
+        name="dadjoke", description="Have Dad tell you one of his classics."
+    )
+    async def dadjoke(
+        self,
+        interaction: Interaction,
+        searchterm: Optional[str] = SlashOption(
+            description="A term to try and find a dadjoke about",
+            default="",
+            required=False,
+        ),
+    ):
         """
         [(Optional)SearchTerm] Have Dad tell you one of his classics.
         """
         if searchterm is None:
             searchterm = ""
-        
+
         url = "https://icanhazdadjoke.com/search?term=" + searchterm
-        headers = {'Accept': 'application/json'}
+        headers = {"Accept": "application/json"}
         r = requests.get(url, headers=headers)
         json = r.json()
         try:
-            await interaction.response.send_message(random.choice(json["results"])["joke"])
+            await interaction.response.send_message(
+                random.choice(json["results"])["joke"]
+            )
         except:
-            await interaction.response.send_message("I don't think I've heard a good one about that yet. Try something else.")
-    
+            await interaction.response.send_message(
+                "I don't think I've heard a good one about that yet. Try something else."
+            )
+
     @nextcord.slash_command(name="xkcd", description="Get an xkcd comic.")
-    async def xkcd(self, interaction: Interaction, comicnumber: Optional[int] = SlashOption(description="A specific xkcd comic, like '1' to get the first comic", default="", required=False)):
+    async def xkcd(
+        self,
+        interaction: Interaction,
+        comicnumber: Optional[int] = SlashOption(
+            description="A specific xkcd comic, like '1' to get the first comic",
+            default="",
+            required=False,
+        ),
+    ):
         """
         [(Optional)xkcdNumber] Retrieve a random or specific xkcd comic
         """
         r = requests.get("http://xkcd.com/info.0.json")
-        comicnumber = comicnumber if comicnumber != "" else str(random.choice(range(1, r.json()['num']))) # type: ignore
+        comicnumber = comicnumber if comicnumber != "" else str(random.choice(range(1, r.json()["num"])))  # type: ignore
 
         r = requests.get("http://xkcd.com/" + str(comicnumber) + "/info.0.json")
 
         try:
-            await interaction.response.send_message(r.json()['img'])
+            await interaction.response.send_message(r.json()["img"])
         except:
-            await interaction.response.send_message("I can't find that xkcd comic, try another.")
+            await interaction.response.send_message(
+                "I can't find that xkcd comic, try another."
+            )
 
-    @nextcord.slash_command(name="apod", description="Get the astonomy picture of the day from NASA.")
+    @nextcord.slash_command(
+        name="apod", description="Get the astonomy picture of the day from NASA."
+    )
     async def apod(self, interaction: Interaction):
         """
         Get the NASA picture of the day
         """
         await interaction.response.defer()
         self.bot.logger.info("Getting NASA picture of the day")
-        response = requests.get("https://api.nasa.gov/planetary/apod?api_key=hQqgupM0Ghb1OTjjrPkoIDw1EJq6pZQQdgMGBpnb")
+        response = requests.get(
+            "https://api.nasa.gov/planetary/apod?api_key=hQqgupM0Ghb1OTjjrPkoIDw1EJq6pZQQdgMGBpnb"
+        )
 
         if response.status_code != 200 or len(response.json()) == 0:
-            await interaction.followup.send("NASA APOD is currently down, please try again later")
+            await interaction.followup.send(
+                "NASA APOD is currently down, please try again later"
+            )
             self.bot.logger.error("NASA APOD is down")
             return
-        
+
         if response.json()["media_type"] == "video":
-            embed = Embed(title="Astronomy Picture of the Day", description=response.json()["explanation"])
+            embed = Embed(
+                title="Astronomy Picture of the Day",
+                description=response.json()["explanation"],
+            )
             embed.set_image(url=response.json()["thumbnail_url"])
             await interaction.followup.send(embed=embed)
             return
 
-        embed = Embed(title="Astronomy Picture of the Day", description=response.json()["explanation"])
+        embed = Embed(
+            title="Astronomy Picture of the Day",
+            description=response.json()["explanation"],
+        )
         embed.set_image(url=response.json()["hdurl"])
         await interaction.followup.send(embed=embed)
-    
-    @nextcord.slash_command(name="iswanted", description="See if someone is on the FBI's most wanted list.")
-    async def iswanted(self, interaction: Interaction, name: Optional[str] = SlashOption(description="The name of the person you want to check", required=True)):
+
+    @nextcord.slash_command(
+        name="iswanted", description="See if someone is on the FBI's most wanted list."
+    )
+    async def iswanted(
+        self,
+        interaction: Interaction,
+        name: Optional[str] = SlashOption(
+            description="The name of the person you want to check", required=True
+        ),
+    ):
         """
         [SearchTerm] See if someone is on the FBI's most wanted list.
         """
         r = requests.get("https://api.fbi.gov/wanted/v1/list", params={"title": name})
 
         try:
-            url = random.choice(r.json()['items'])["files"][0]['url']
+            url = random.choice(r.json()["items"])["files"][0]["url"]
             if url is None:
                 url = ""
-                
+
             if name is None:
-                await interaction.response.send_message("No one with that name is currently wanted by the FBI")
+                await interaction.response.send_message(
+                    "No one with that name is currently wanted by the FBI"
+                )
                 return
-            
-            await interaction.response.send_message(name + " might be wanted by the FBI:\n" + url)
+
+            await interaction.response.send_message(
+                name + " might be wanted by the FBI:\n" + url
+            )
         except:
-            await interaction.response.send_message("No one with that name is currently wanted by the FBI")
-    
-    @nextcord.slash_command(name="roastme", description="Dad's been around the block a few times, give him a try.")
+            await interaction.response.send_message(
+                "No one with that name is currently wanted by the FBI"
+            )
+
+    @nextcord.slash_command(
+        name="roastme",
+        description="Dad's been around the block a few times, give him a try.",
+    )
     async def roastme(self, interaction: Interaction):
         """
         [No Arguments] Dad's been around the block a few times, give him a try.
@@ -114,29 +169,31 @@ class Fun(commands.Cog, name="fun"):
         r = requests.get(url)
         json = r.json()
         await interaction.response.send_message(json["insult"])
-    
+
     @nextcord.slash_command(name="eightball", description="Ask any question to dad.")
-    async def eight_ball(self, interaction: Interaction, question: Optional[str] = SlashOption(description="The question you want to ask dad", required=True)):
+    async def eight_ball(
+        self,
+        interaction: Interaction,
+        question: Optional[str] = SlashOption(
+            description="The question you want to ask dad", required=True
+        ),
+    ):
         """
         [Question] Ask any question to the bot.
         """
-        answers = ['It is certain.', 'It is decidedly so.', 'You may rely on it.', 'Without a doubt.',
-                   'Yes - definitely.', 'As I see, yes.', 'Most likely.', 'Outlook good.', 'Yes.',
-                   'Signs point to yes.', 'Reply hazy, try again.', 'Ask again later.', 'Better not tell you now.',
-                   'Cannot predict now.', 'Concentrate and ask again later.', 'Don\'t count on it.', 'My reply is no.',
-                   'My sources say no.', 'Outlook not so good.', 'Very doubtful.']
-        embed = nextcord.Embed(
-            title="**My Answer:**",
-            description=f"{answers[random.randint(0, len(answers) - 1)]}",
-            color=config["success"]
-        )
-        embed.set_footer(
-            text=f"Question asked by: {interaction.user}"
+        # Use dadroid_single to get a response from the chat model, but make it funny
+        await dadroid_single(
+            "You are a bot that acts similar to a magic 8 ball, but your answers are specific to the question. Your goal is to answer the following question. It should be a relatively short, but also funny answer. The answer can be in the positive, negative, or neutral. Make sure your answer is specific to the question. Do not start the answer with 'outlook', and make sure the answer is either yes or no.",
+            question,
+            first_send_method=interaction.response.send_message,
+            send_method=interaction.followup.send,
+            response_starter=f"{question}\n >>> ",
+            beef=True,
         )
 
-        await interaction.response.send_message(embed=embed)
-
-    @nextcord.slash_command(name="bitcoin", description="Get the current price of bitcoin.")
+    @nextcord.slash_command(
+        name="bitcoin", description="Get the current price of bitcoin."
+    )
     async def bitcoin(self, interaction: Interaction):
         """
         [No Arguments] Get the current price of bitcoin.
@@ -150,9 +207,10 @@ class Fun(commands.Cog, name="fun"):
             embed = nextcord.Embed(
                 title=":information_source: Info",
                 description=f"Bitcoin price is: ${response['bpi']['USD']['rate']}",
-                color=config["success"]
+                color=config["success"],
             )
             await interaction.response.send_message(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(Fun(bot))

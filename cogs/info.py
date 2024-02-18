ac032f2d128d4094baab484f0ad633a9ba68b21c
Colin Wilson <github@colinwilson.dev>
2024-02-18 16:26:33 -0500

    fix: :bug: Remove admin perms from invite link

--- cogs/info.py
+++ cogs/info.py
@@ -12,51 +12,42 @@ from nextcord.abc import GuildChannel
 with open("config.yaml") as file:
     config = yaml.load(file, Loader=yaml.FullLoader)
 
+
 # Here we name the cog and create a new class for the cog.
 class Info(commands.Cog, name="info"):
     def __init__(self, bot):
         self.bot = bot
 
-    @nextcord.slash_command(name="info", description="Get some useful (or not) information about the bot.")
+    @nextcord.slash_command(
+        name="info", description="Get some useful (or not) information about the bot."
+    )
     async def info(self, interaction: Interaction):
         """
         [No Arguments] Get some useful (or not) information about the bot.
         """
-        embed = nextcord.Embed(
-            description="The server's dad",
-            color=config["success"]
-        )
-        embed.set_author(
-            name="Bot Information"
-        )
+        embed = nextcord.Embed(description="The server's dad", color=config["success"])
+        embed.set_author(name="Bot Information")
+        embed.add_field(name="Owner:", value="nanosplitter", inline=True)
         embed.add_field(
-            name="Owner:",
-            value="nanosplitter",
-            inline=True
+            name="Python Version:", value=f"{platform.python_version()}", inline=True
         )
-        embed.add_field(
-            name="Python Version:",
-            value=f"{platform.python_version()}",
-            inline=True
-        )
-        embed.add_field(
-            name="Prefix:",
-            value=f"{config['bot_prefix']}",
-            inline=False
-        )
-        embed.set_footer(
-            text=f"Requested by {interaction.user}"
-        )
+        embed.add_field(name="Prefix:", value=f"{config['bot_prefix']}", inline=False)
+        embed.set_footer(text=f"Requested by {interaction.user}")
         await interaction.response.send_message(embed=embed)
 
-    @nextcord.slash_command(name="serverinfo", description="Get some useful (or not) information about the server.")
+    @nextcord.slash_command(
+        name="serverinfo",
+        description="Get some useful (or not) information about the server.",
+    )
     async def serverinfo(self, interaction: Interaction):
         """
         [No Arguments] Get some useful (or not) information about the server.
         """
         server = interaction.guild
         if server is None:
-            await interaction.response.send_message("Sorry, I can't find your server information.")
+            await interaction.response.send_message(
+                "Sorry, I can't find your server information."
+            )
             return
         roles = [x.name for x in server.roles]
         role_length = len(roles)
@@ -70,34 +61,16 @@ class Info(commands.Cog, name="info"):
         time = time[0]
 
         embed = nextcord.Embed(
-            title="**Server Name:**",
-            description=f"{server}",
-            color=config["success"]
+            title="**Server Name:**", description=f"{server}", color=config["success"]
         )
 
         if server.icon != None:
-            embed.set_thumbnail(
-                url=server.icon.url
-            )
-        embed.add_field(
-            name="Server ID",
-            value=server.id
-        )
-        embed.add_field(
-            name="Member Count",
-            value=server.member_count
-        )
-        embed.add_field(
-            name="Text/Voice Channels",
-            value=f"{channels}"
-        )
-        embed.add_field(
-            name=f"Roles ({role_length})",
-            value=roles
-        )
-        embed.set_footer(
-            text=f"Created at: {time}"
-        )
+            embed.set_thumbnail(url=server.icon.url)
+        embed.add_field(name="Server ID", value=server.id)
+        embed.add_field(name="Member Count", value=server.member_count)
+        embed.add_field(name="Text/Voice Channels", value=f"{channels}")
+        embed.add_field(name=f"Roles ({role_length})", value=roles)
+        embed.set_footer(text=f"Created at: {time}")
         await interaction.response.send_message(embed=embed)
 
     @nextcord.slash_command(name="ping", description="Check if the bot is alive.")
@@ -105,25 +78,23 @@ class Info(commands.Cog, name="info"):
         """
         [No Arguments] Check if the bot is alive.
         """
-        embed = nextcord.Embed(
-            color=config["success"]
-        )
-        embed.add_field(
-            name="Pong!",
-            value=":ping_pong:",
-            inline=True
-        )
-        embed.set_footer(
-            text=f"Pong request by {interaction.user}"
-        )
-        await interaction.response.send_message(embed=embed)  
+        embed = nextcord.Embed(color=config["success"])
+        embed.add_field(name="Pong!", value=":ping_pong:", inline=True)
+        embed.set_footer(text=f"Pong request by {interaction.user}")
+        await interaction.response.send_message(embed=embed)
 
-    @nextcord.slash_command(name="invite", description="Get the invite link of the Dad to be able to invite him to another server.")
+    @nextcord.slash_command(
+        name="invite",
+        description="Get the invite link of the Dad to be able to invite him to another server.",
+    )
     async def invite(self, interaction: Interaction):
         """
         [No Arguments] Get the invite link of the Dad to be able to invite him to another server.
         """
-        await interaction.response.send_message(f"Invite me by clicking here: https://discordapp.com/oauth2/authorize?&client_id={config['application_id']}&scope=bot&permissions=8")
+        await interaction.response.send_message(
+            f"Invite me by clicking here: https://discordapp.com/oauth2/authorize?&client_id={config['application_id']}&scope=bot"
+        )
+
 
 # And then we finally add the cog to the bot so that it can load, unload, reload and use it's content.
 def setup(bot):

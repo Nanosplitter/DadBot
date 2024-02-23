5629b825428f5c95ad5ac32964fe6a2d9c3330a2
Colin Wilson <github@colinwilson.dev>
2024-02-22 19:32:07 -0500

    feat: :sparkles: Add automatic reconnect logic to DB

--- services/db_service.py
+++ services/db_service.py
@@ -1,10 +1,37 @@
-from peewee import *
+from peewee import MySQLDatabase
+from pymysql import OperationalError
 import yaml
 
+# Load your configuration
 with open("config.yaml") as file:
     config = yaml.load(file, Loader=yaml.FullLoader)
 
 
-def get_db():
-    return MySQLDatabase(config["databasename"], user=config["dbuser"], password=config["dbpassword"], host=config["dbhost"], port=3306)
+class ReconnectMySQLDatabase(MySQLDatabase):
+    def execute_sql(self, sql, params=None, commit=True):
+        print("Executing SQL query.", sql, params, commit)
+        try:
+            return super().execute_sql(sql, params, commit)
+        except OperationalError as e:
+            if e.args[0] in (
+                2006,
+                2013,
+            ):
+                print(
+                    "Database connection lost, attempting to reconnect and retry the query."
+                )
+                self.connect(reuse_if_open=True)
+                return super().execute_sql(sql, params, commit)
+            else:
+                raise
 
+
+# Updated get_db function to use the custom database class
+def get_db():
+    return ReconnectMySQLDatabase(
+        config["databasename"],
+        user=config["dbuser"],
+        password=config["dbpassword"],
+        host=config["dbhost"],
+        port=3306,
+    )

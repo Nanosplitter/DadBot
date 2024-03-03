df2a737e494b10e4178e3d5ecb0d9cd2c5fe84f2
Colin Wilson <github@colinwilson.dev>
2024-03-03 01:06:07 -0500

    fix: :bug: Add ability for DB to reconnect whenever connection is lost

--- services/db_service.py
+++ services/db_service.py
@@ -1,8 +1,6 @@
 from peewee import MySQLDatabase
-from pymysql import OperationalError
 import yaml
 
-# Load your configuration
 with open("config.yaml") as file:
     config = yaml.load(file, Loader=yaml.FullLoader)
 
@@ -11,22 +9,12 @@ class ReconnectMySQLDatabase(MySQLDatabase):
     def execute_sql(self, sql, params=None, commit=True):
         try:
             return super().execute_sql(sql, params, commit)
-        except OperationalError as e:
-            print(f"An OperationalError occurred: {e}")
-            if e.args[0] in (
-                2006,
-                2013,
-            ):
-                print(
-                    "Database connection lost, attempting to reconnect and retry the query."
-                )
-                self.connect(reuse_if_open=True)
-                return super().execute_sql(sql, params, commit)
-            else:
-                raise
         except Exception as e:
-            print(f"An error occurred: {e}")
-            raise
+            print(
+                "Database connection lost, attempting to reconnect and retry the query."
+            )
+            self.connect(reuse_if_open=True)
+            return super().execute_sql(sql, params, commit)
 
 
 def get_db():

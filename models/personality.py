01090c29eab21959498348e6fdd6dae7f5c27ff8
Colin Wilson <github@colinwilson.dev>
2024-03-12 11:04:59 -0400

    fix: :bug: Fix embed for personality save of long personalities

--- models/personality.py
+++ models/personality.py
@@ -3,6 +3,7 @@ from services.db_service import get_db
 from nextcord import Interaction, Embed, ButtonStyle
 from nextcord.ui import Button
 
+
 class Personality(Model):
     id = AutoField()
     user_id = CharField()
@@ -11,32 +12,40 @@ class Personality(Model):
 
     class Meta:
         database = get_db()
-    
+
     def __str__(self):
         return f"Personality: {self.name} ({self.id})"
-    
+
     def __repr__(self):
         return f"Personality: {self.name} ({self.id})"
 
     def make_embed(self):
         embed = Embed(title=self.name)
-        embed.add_field(name="Personality", value=f"{self.personality}", inline=False)
+        embed.add_field(
+            name="Personality", value=f"{self.personality[:1024]}", inline=False
+        )
 
         return embed
-    
+
+
 def get_saved_personalities(user_id):
     return Personality.select().where(Personality.user_id == user_id)
 
+
 def get_personality(user_id, name):
-    return Personality.get_or_none((Personality.user_id == user_id) & (Personality.name == name))
+    return Personality.get_or_none(
+        (Personality.user_id == user_id) & (Personality.name == name)
+    )
+
 
 def delete_personality(row_id):
     personality = Personality.get_or_none((Personality.id == row_id))
     if personality is not None:
         personality.delete_instance()
         return True
-    
+
     return False
+
 
 class DeleteButton(Button):
     def __init__(self, row_id, user_id):
@@ -46,11 +55,17 @@ class DeleteButton(Button):
 
     async def callback(self, interaction: Interaction):
         if interaction.user.id != self.user_id:
-            await interaction.response.send_message("You can't delete someone else's book!", ephemeral=True)
+            await interaction.response.send_message(
+                "You can't delete someone else's book!", ephemeral=True
+            )
             return
-        
+
         if delete_personality(self.row_id):
-            await interaction.response.send_message("Personality deleted.", ephemeral=True)
+            await interaction.response.send_message(
+                "Personality deleted.", ephemeral=True
+            )
             await interaction.message.delete()
         else:
-            await interaction.response.send_message("I couldn't delete that personality.", ephemeral=True)
+            await interaction.response.send_message(
+                "I couldn't delete that personality.", ephemeral=True
+            )

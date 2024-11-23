bf2d27cba6df5700d9d7cc7563463dee85dca469
Colin Wilson <github@colinwilson.dev>
2024-11-22 22:20:22 -0500

    feat: :sparkles: Make remove paywall support multiple links in one message

--- cogs/remove_paywall.py
+++ cogs/remove_paywall.py
@@ -1,4 +1,5 @@
 import re
+import urllib.parse
 import nextcord
 from nextcord.ext import commands
 
@@ -6,18 +7,22 @@ class RemovePaywall(commands.Cog, name="remove_paywall"):
     def __init__(self, bot):
         self.bot = bot
 
+    def create_link_buttons(self, urls):
+        view = nextcord.ui.View()
+        for url in urls:
+            parsed_url = urllib.parse.urlparse(url)
+            domain = parsed_url.netloc.replace('www.', '')
+            paywall_removed_link = f"https://removepaywall.com/{url}"
+            button = nextcord.ui.Button(label=domain, url=paywall_removed_link)
+            view.add_item(button)
+        return view
+
     @nextcord.message_command(name="remove paywall")
     async def remove_paywall(self, interaction: nextcord.Interaction, message: nextcord.Message):
         pattern = r'(https?://\S+)'
-        match = re.search(pattern, message.content)
-        if match:
-            link = match.group(0)
-            paywall_removed_link = f"https://removepaywall.com/{link}"
-
-            button = nextcord.ui.Button(label="Open Paywall-Removed Link", url=paywall_removed_link)
-            view = nextcord.ui.View()
-            view.add_item(button)
-
+        urls = re.findall(pattern, message.content)
+        if urls:
+            view = self.create_link_buttons(urls)
             await interaction.response.send_message("", view=view)
         else:
             await interaction.response.send_message("No link found in the message.", ephemeral=True)
